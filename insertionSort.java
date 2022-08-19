e810186d354aab03c693cb916d06e1ae6b2012ac
Rohan Kumar <crohank02@gmail.com>
2022-08-19 10:48:37 +0530

    Add files via upload

---
+++ insertionSort.java
@@ -0,0 +1,24 @@
+
+public class insertionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = new int[]{9,18,3,2,10};
+		sort(arr);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	static void sort(int arr[]) {
+		int n = arr.length;
+		for(int i=1;i<n;i++) {
+			int j = i-1;
+			int t = arr[i];
+			while(j>=0 && arr[j]>t) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1]=t;
+	}
+}
+}
